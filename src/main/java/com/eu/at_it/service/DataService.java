2d03077a83dc891f6ea6745deeabab2d05f782a9
asutalo <a.sutalo@hotmail.com>
2021-10-21 15:10:07 +0100

    added couple more convenience methods to the data service interface

--- src/main/java/com/eu/at_it/service/DataService.java
+++ src/main/java/com/eu/at_it/service/DataService.java
@@ -1,6 +1,7 @@
 package com.eu.at_it.service;
 
 import java.util.List;
+import java.util.Map;
 
 public interface DataService<T, Q> {
     T update(T toUpdate) throws Exception;
@@ -13,7 +14,13 @@ public interface DataService<T, Q> {
 
     T get(Q filteredSelect) throws Exception;
 
+    T get(Map<String, Object> filter) throws Exception;
+
     List<T> getAll() throws Exception;
 
+    List<T> getAll(Map<String, Object> filter) throws Exception;
+
     List<T> getAll(Q filteredSelect) throws Exception;
+
+    T instanceOfT(Map<String, Object> values);
 }
