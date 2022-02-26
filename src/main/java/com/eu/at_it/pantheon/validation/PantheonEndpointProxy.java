4afd19dc5d4a9a4a2cd23c07f4f9edbc08d28f27
asutalo <a.sutalo@hotmail.com>
2022-02-26 16:35:30 +0000

    generified the PantheonEndpointProxy

--- src/main/java/com/eu/at_it/pantheon/validation/PantheonEndpointProxy.java
+++ src/main/java/com/eu/at_it/pantheon/validation/PantheonEndpointProxy.java
@@ -6,8 +6,8 @@ import java.lang.reflect.Proxy;
 import java.util.Map;
 
 public class PantheonEndpointProxy {
-    public PantheonEndpoint getProxyFor(PantheonEndpoint endpoint, Map<String, EndpointValidator> endpointMethodsAndValidators) {
-        return (PantheonEndpoint) Proxy.newProxyInstance(endpoint.getClass().getClassLoader(),
+    public <T extends PantheonEndpoint> T getProxyFor(T endpoint, Map<String, EndpointValidator> endpointMethodsAndValidators) {
+        return (T) Proxy.newProxyInstance(endpoint.getClass().getClassLoader(),
                 new Class[]{PantheonEndpoint.class}, new EndpointRequestValidator(endpoint, endpointMethodsAndValidators));
     }
 }
